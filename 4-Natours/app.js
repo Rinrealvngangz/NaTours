d319a68d4e0ef03b5b2cd06eb888668d15b916ba
NguyenMauTuan <49154074+Rinrealvngangz@users.noreply.github.com>
2020-04-17 10:52:15 +0700

    Add files via upload
    error handle global

--- 4-Natours/app.js
+++ 4-Natours/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandle = require('./controller/errorController');
 const toursRouter = require('./routes/toursRoutes');
 const usersRouter = require('./routes/usersRoutes');
 
@@ -27,5 +29,18 @@ app.use((req, res, next) => {
 //2) Router
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server !`,
+  // });
+  // const err = new Error(`Can't find ${req.originalUrl} on this server !`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  next(new AppError(`Can't find ${req.originalUrl} on this server !`, 404));
+});
+
+app.use(globalErrorHandle);
 
 module.exports = app;
