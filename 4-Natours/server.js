86dc960904a4ce8df0ffaaf832d4bbef23803366
NguyenMauTuan <49154074+Rinrealvngangz@users.noreply.github.com>
2020-04-21 18:41:56 +0700

    Add files via upload

--- 4-Natours/server.js
+++ 4-Natours/server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNHANDLE EXCEPTION... Shuting down');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -18,6 +24,16 @@ mongoose
 
 const port = process.env.PORT || 3000;
 //console.log(process.env);
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}....`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLE REJECTION... Shuting down');
+
+  console.log(err.name, err.message);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
