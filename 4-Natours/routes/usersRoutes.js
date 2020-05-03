bbaa76572fe43db37334f0c4a60e71ea21f07e3d
NguyenMauTuan <49154074+Rinrealvngangz@users.noreply.github.com>
2020-05-03 17:50:31 +0700

    Add files via upload
    add router sign up

--- 4-Natours/routes/usersRoutes.js
+++ 4-Natours/routes/usersRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const Router = express.Router();
-const usersCotroller = require('./../controller/usersController');
-Router.route('/')
-  .get(usersCotroller.getAllUser)
-  .post(usersCotroller.createUser);
 
-Router.route('/:id')
-  .get(usersCotroller.getUser)
-  .patch(usersCotroller.updateUser)
-  .delete(usersCotroller.deleteUser);
+const usersCotroller = require('../controller/usersController');
+const authController = require('../controller/authController');
+
+const Router = express.Router();
+
+Router.post('/signup', authController.singup);
+
+Router.route('/').get(usersCotroller.getAllUser).post(usersCotroller.createUser);
+
+Router.route('/:id').get(usersCotroller.getUser).patch(usersCotroller.updateUser).delete(usersCotroller.deleteUser);
 module.exports = Router;
