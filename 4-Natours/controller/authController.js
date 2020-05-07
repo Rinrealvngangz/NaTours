2923336731e6f1d03380f9c60766125a0de51429
NguyenMauTuan <49154074+Rinrealvngangz@users.noreply.github.com>
2020-05-07 10:44:21 +0700

    Add files via upload

--- 4-Natours/controller/authController.js
+++ 4-Natours/controller/authController.js
@@ -1,10 +1,12 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.singup = catchAsync(async (req, res, next) => {
   const newsUser = await User.create(req.body);
-
+  const token = jwt.sign({ id: newsUser._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
   res.status(201).json({
+    token,
     status: 'success',
     data: {
       user: newsUser,
