81067aa98d8f6a496ba5713240fc9bd09dd7b7c0
NguyenMauTuan <49154074+Rinrealvngangz@users.noreply.github.com>
2020-05-08 11:07:31 +0700

    Add files via upload

--- 4-Natours/controller/authController.js
+++ 4-Natours/controller/authController.js
@@ -1,15 +1,39 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const signtoken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
+};
 
 exports.singup = catchAsync(async (req, res, next) => {
   const newsUser = await User.create(req.body);
-  const token = jwt.sign({ id: newsUser._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
+  const token = signtoken(newsUser._id);
   res.status(201).json({
     token,
     status: 'success',
     data: {
       user: newsUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError('please provide email and password'), 400);
+  }
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+  const token = signtoken(user._id);
+
+  res.status(200).json({
+    status: 'success',
+    token,
   });
 });
--- 4-Natours/controller/usersController.js
+++ 4-Natours/controller/usersController.js
@@ -1,30 +1,37 @@
-exports.getAllUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUser = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined!'
+    message: 'This route is not yet defined!',
   });
 };
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined!'
+    message: 'This route is not yet defined!',
   });
 };
 exports.updateUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined!'
+    message: 'This route is not yet defined!',
   });
 };
 exports.deleteUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined!'
+    message: 'This route is not yet defined!',
   });
 };
