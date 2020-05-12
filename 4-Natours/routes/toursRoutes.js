e142e9f09e5482f1b8c30dc25a1c6bc666759a2f
NguyenMauTuan <49154074+Rinrealvngangz@users.noreply.github.com>
2020-05-12 22:31:11 +0700

    Add files via upload

--- 4-Natours/routes/toursRoutes.js
+++ 4-Natours/routes/toursRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const toursController = require('../controller/toursController');
+const authController = require('../controller/authController');
 
 const router = express.Router();
 
@@ -7,7 +8,7 @@ const router = express.Router();
 router.route('/top-5-cheap').get(toursController.aliasTopTour, toursController.getallTour);
 router.route('/tour-stats').get(toursController.getTourStats);
 router.route('/monthly-plan/:year').get(toursController.getMonthlyPlan);
-router.route('/').get(toursController.getallTour).post(toursController.createTour);
+router.route('/').get(authController.protect, toursController.getallTour).post(toursController.createTour);
 
 router.route('/:id').get(toursController.getTour).patch(toursController.UpdateTour).delete(toursController.deleteTour);
 
