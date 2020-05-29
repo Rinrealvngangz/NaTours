debeb6d6869aac846860bb4c3bfdb2009a372e07
NguyenMauTuan <49154074+Rinrealvngangz@users.noreply.github.com>
2020-05-29 20:50:49 +0700

    Add files via upload

---
+++ 4-Natours/routes/usersController.js
@@ -0,0 +1,69 @@
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObject = (obj, ...allowerFields) => {
+  const newObj = {};
+//xem thử có phần tử name vs email trong mang ko 
+  Object.keys(obj).forEach((el) => {
+    if (allowerFields.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
+
+exports.getAllUser = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //1)Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('This route not for password update .Please user /updateMyPassword', 400));
+  }
+  //2) filtered out unwanted field names that are not allowed to be updated
+  const filteredBody = filterObject(req.body, 'name', 'email');
+  //3)Update user document
+  const updateUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updateUser,
+    },
+  });
+});
