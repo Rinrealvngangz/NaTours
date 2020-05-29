c5439278f5feb61a07bd5c27b4c25c884e672106
NguyenMauTuan <49154074+Rinrealvngangz@users.noreply.github.com>
2020-05-29 17:04:12 +0700

    Add files via upload

--- 4-Natours/routes/usersRoutes.js
+++ 4-Natours/routes/usersRoutes.js
@@ -6,7 +6,11 @@ const authController = require('../controller/authController');
 const Router = express.Router();
 
 Router.post('/signup', authController.singup);
+Router.post('/login', authController.login);
 
+Router.post('/forgotPassword', authController.forgotPassword);
+Router.patch('/resetPassword/:token', authController.resetPassword);
+Router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
 Router.route('/').get(usersCotroller.getAllUser).post(usersCotroller.createUser);
 
 Router.route('/:id').get(usersCotroller.getUser).patch(usersCotroller.updateUser).delete(usersCotroller.deleteUser);
