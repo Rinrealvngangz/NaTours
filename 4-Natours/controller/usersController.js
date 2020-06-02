595e37271290a32f45c0e7f96052d9cad9041daa
NguyenMauTuan <49154074+Rinrealvngangz@users.noreply.github.com>
2020-06-02 20:54:32 +0700

    Add files via upload

--- 4-Natours/controller/usersController.js
+++ 4-Natours/controller/usersController.js
@@ -1,5 +1,16 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObject = (obj, ...allowerFields) => {
+  const newObj = {};
+  //xem thử có phần tử name vs email trong mang ko
+  Object.keys(obj).forEach((el) => {
+    if (allowerFields.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
 
 exports.getAllUser = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -35,3 +46,32 @@ exports.deleteUser = (req, res) => {
     message: 'This route is not yet defined!',
   });
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //1)Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('This route not for password update .Please user /updateMyPassword', 400));
+  }
+  //2) filtered out unwanted field names that are not allowed to be updated
+  const filteredBody = filterObject(req.body, 'name', 'email');
+  //3)Update user document
+  const updateUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updateUser,
+    },
+  });
+});
+
+exports.DeleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
