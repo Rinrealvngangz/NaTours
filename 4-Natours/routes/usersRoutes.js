7ae6cb5079e2241409ee56e9b65e38856ebd4402
NguyenMauTuan <49154074+Rinrealvngangz@users.noreply.github.com>
2020-06-02 20:47:35 +0700

    Add files via upload

--- 4-Natours/routes/usersRoutes.js
+++ 4-Natours/routes/usersRoutes.js
@@ -11,6 +11,8 @@ Router.post('/login', authController.login);
 Router.post('/forgotPassword', authController.forgotPassword);
 Router.patch('/resetPassword/:token', authController.resetPassword);
 Router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
+Router.patch('/updateMe', authController.protect, usersCotroller.updateMe);
+Router.delete('/deleteMe', authController.protect, usersCotroller.DeleteMe);
 Router.route('/').get(usersCotroller.getAllUser).post(usersCotroller.createUser);
 
 Router.route('/:id').get(usersCotroller.getUser).patch(usersCotroller.updateUser).delete(usersCotroller.deleteUser);
