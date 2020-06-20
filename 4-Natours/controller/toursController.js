9918411278a6117642ac429eafb248b87f411f2c
NguyenMauTuan <49154074+Rinrealvngangz@users.noreply.github.com>
2020-06-20 21:53:02 +0700

    Add files via upload

--- 4-Natours/controller/toursController.js
+++ 4-Natours/controller/toursController.js
@@ -122,3 +122,41 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+
+  const [lat, lng] = latlng.split(',');
+
+  if (!lat || !lng) {
+    next(new AppError('Please provide latitur and longtitude in the format lat and lng. ', 400));
+  }
+  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1],
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multiplier,
+      },
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1,
+      },
+    },
+  ]);
+  res.status(200).json({
+    status: 'success',
+
+    data: {
+      data: distances,
+    },
+  });
+
+  next();
+});
