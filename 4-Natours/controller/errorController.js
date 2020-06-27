e1542efbe451bfc702dbfb81af3ed4cd1eccc18e
NguyenMauTuan <Rinrealvngangz>
2020-06-27 11:13:08 +0700

    add render error page

--- 4-Natours/controller/errorController.js
+++ 4-Natours/controller/errorController.js
@@ -20,47 +20,74 @@ const handleValidationErrorDB = (err) => {
   const message = `Invalid input data. ${errors.join('. ')}`;
   return new AppError(message, 400);
 };
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stacks: err.stacks,
+const sendErrorDev = (err, req, res) => {
+  if (req.originalUrl.startsWith('/api')) {
+    // API
+    return res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stacks: err.stacks,
+    });
+  } //REDERRED WEBSITE
+  console.error('ERROR...', err);
+
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong',
+    msg: err.message,
   });
 };
-const sendErrorProd = (err, res) => {
-  // Operational trusted error:send message to client
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
-  }
-  //Programing or orther unknwon error:don't leak error details
-  else {
+const sendErrorProd = (err, req, res) => {
+  //A) API
+  if (req.originalUrl.startsWith('/api')) {
+    //A) Operational trusted error:send message to client
+    if (err.isOperational) {
+      res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
+    // B )Programing or orther unknwon error:don't leak error details
     //1)Log
     console.error('ERROR...', err);
     //2)Send generic message
-    res.status(500).json({
+    return res.status(500).json({
       status: 'error',
       message: 'Something went very wrong!',
     });
   }
+  //B) REder Website
+  if (err.isOperational) {
+    return res.status(err.statusCode).render('error', {
+      title: 'Something went wrong',
+      msg: err.message,
+    });
+  }
+  console.error('ERROR...', err);
+  //2)Send generic message
+  return res.status(err.statusCode).render('error', {
+    title: 'Please try again later.',
+    msg: err.message,
+  });
 };
+
 module.exports = (err, req, res, next) => {
   //console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+    sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
+    error.message = err.message;
+
     if (error.name === 'CastError') error = HadleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
     if (error.name === 'JsonWebTokenError') error = handleJWTError();
     if (error.name === 'TokenExpiredError') error = habdleJWTExpiredError();
-    sendErrorProd(error, res);
+
+    sendErrorProd(error, req, res);
   }
 };
--- 4-Natours/public/js/bundle.js
+++ 4-Natours/public/js/bundle.js
@@ -8824,7 +8824,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49498" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49406" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
